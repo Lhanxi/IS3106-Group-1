35efcba689d028eed5f56480ffc32ce2002a0273
Jeremythong2002 <jeremythong2002@gmail.com>
2025-02-25 23:10:09 +0800

    basic layout of each of the card, without styling yet

---
+++ frontend/src/components/KanbanCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const KanbanCard = ({title}) => {
+
+    return (
+        //only displays the name of the task for now 
+    <div>
+        {title}
+    </div>
+    );
+}
+
+export default KanbanCard
